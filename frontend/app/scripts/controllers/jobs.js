33f0752db2a17a3d228055d299cb712d9a9655a1
Benjamin White <whtouche@Benjamins-MacBook-Pro.local>
2015-08-05 23:12:21 -0400

    create jobs api in sails

--- frontend/app/scripts/controllers/jobs.js
+++ frontend/app/scripts/controllers/jobs.js
@@ -8,16 +8,9 @@
 * Controller of the ngNodeJwtApp
 */
 angular.module('ngNodeJwtApp').controller('JobsCtrl', function ($scope, $http, API_URL, alert) {
-    $http.get(API_URL + 'jobs')
-    .success(function(jobs) {
+    $http.get('http://localhost:1337/job').success(function(jobs) {
         $scope.jobs = jobs;
-    })
-    .error(function(err) {
+    }).error(function(err) {
         alert('warning', 'Unable to get jobs', err.message);
     })
-    $scope.jobs = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
-    ];
 });
